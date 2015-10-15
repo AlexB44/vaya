036dad46162e20e0c48072d98ed8f3d62b2f1241
Balatzar <bataprod@gmail.com>
2015-10-15 12:06:27 +0200

    fixed data global

--- controllers/formController.js
+++ controllers/formController.js
@@ -2,17 +2,22 @@ function formController($scope, $http) {
   $scope.title = "Accueil";
   var data = {};
   
+  function updateDOM(dato) {
+    data = dato;
+    $scope.title = dato.name;
+    console.log(data);
+  }
+  
   $scope.sendForm = function() {
     if (!$scope.form || $scope.form === "") { return }
     var location = $scope.form;
-    $scope.title = location;
     $scope.form = "";
     $http({
       method: "GET",
       url: "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=39583154fb47ae5a44360b9c46bb99d2"
     }).then(function sucess(res) {
       data = res.data;
-      console.log(data);
+      updateDOM(data);
     }, function fail(res) {
       console.log("error : " + res)
     });
