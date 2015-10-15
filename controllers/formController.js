3eccd54d6e2c39ce0eff22e3a3db973c256804a4
Balatzar <bataprod@gmail.com>
2015-10-15 11:50:28 +0200

    request works

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,10 +1,20 @@
 function formController($scope, $http) {
   $scope.title = "Accueil";
+  var data = {};
   
   $scope.sendForm = function() {
     if (!$scope.form || $scope.form === "") { return }
     var location = $scope.form;
     $scope.title = location;
     $scope.form = "";
+    $http({
+      method: "GET",
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=39583154fb47ae5a44360b9c46bb99d2"
+    }).then(function sucess(res) {
+      data = res.data;
+      console.log(data);
+    }, function fail(res) {
+      console.log("error : " + res)
+    });
   }
 }
